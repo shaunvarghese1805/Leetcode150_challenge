bf1348d748e3a04b2281680111950baac54aa413
Shaun Jacob Varghese <shaun.varghese1805@gmail.com>
2023-04-02 17:02:35 +0530

    Update Contains_duplicates.py
    Solution to the problem

--- Contains_duplicates.py
+++ Contains_duplicates.py
@@ -1,2 +1,12 @@
 #If array nums contains atleat one pair of duplicates, return true. Else return False 
+
 nums = [1,2,3,4,1]
+
+def containsDuplicate(self, nums: list[int]) -> bool:
+    hashset = set()
+    
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
