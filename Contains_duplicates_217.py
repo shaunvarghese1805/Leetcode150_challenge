bc052010f746f30bb5a15ebf4e26bc77b1a4d9b3
Shaun Jacob Varghese <shaun.varghese1805@gmail.com>
2023-04-10 23:10:13 +0530

    Update Contains_duplicates_217.py
    Added a few more descriptors for personal reference

--- Contains_duplicates_217.py
+++ Contains_duplicates_217.py
@@ -2,12 +2,15 @@
 
 
 def containsDuplicate(nums):
+    #here we use set to check if the element is already present in the input
     hashset = set()
     
     for n in nums:
+        #if the element is already present in the set then return true
         if n in hashset:
             return True
         hashset.add(n)
+    #returns false after going through the entire array and exiting the loop
     return False
 
 print(containsDuplicate([1,2,3,4,1]))
