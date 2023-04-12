7748c4d8e42b48a86fc17e97622d6d7a5022f2f5
Shaun Jacob Varghese <shaun.varghese1805@gmail.com>
2023-04-12 23:50:10 +0530

    Create Valid_Palindrome_125.py
    Solution to problem 125

---
+++ Valid_Palindrome_125.py
@@ -0,0 +1,13 @@
+#Given a string s, return true if it is a palindrome, or false otherwise.
+
+def isPalindrome(s):
+    newstr = ""
+
+    for n in s:
+        if n.isalnum():
+            newstr+= n.lower()
+
+        inv_newstr = newstr[::-1]
+        return newstr == inv_newstr
+           
+#Test cases
